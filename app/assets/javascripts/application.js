3bf6d86eef7a7bf27a8c98f63db3ac8424f2ae79
Bryan <bryan.knouse@gmail.com>
2014-11-06 13:10:27 -0500

    testing new font

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,176 @@
 //= require_tree .
 
 
+
+
+//TIMER
+
+function infinite_timer(item, time) {
+	setTimeout(function () {
+		infinite(item);
+	}, time);
+}
+
+//TIME CONTROL
+function infinite(item) {
+	flicker(item, 2, function () {
+		$(item).fadeIn("fast");
+	})
+
+	var numeral = Math.floor((Math.random()*20000)+1);
+	
+	infinite_timer(item, numeral);	
+}
+
+
+
+// FLICKER FUNCTIONS
+
+function flicker(item, count, callback, current) {
+    current = current || 0;
+    
+    $(item)[current % 2 == 0 ? 'visible' : 'invisible']();
+    
+    setTimeout(function(){
+        if (count * 2 <= current) {
+            callback();
+            return;
+        }
+        flicker(item, count, callback, current + 1)
+    }, 40);
+}
+
+function flicker_spark(item, count, callback, current) {
+    current = current || 0;
+    
+    $(item)[current % 2 == 0 ? 'visible' : 'invisible']();
+    
+    setTimeout(function(){
+        if (count * 2 <= current) {
+            callback();
+            return;
+        }
+        flicker_spark(item, count, callback, current + 1)
+    }, 60);
+}
+
+function flicker_quick(item, count, callback, current) {
+    current = current || 0;
+    
+    $(item)[current % 2 == 0 ? 'visible' : 'invisible']();
+    
+    setTimeout(function(){
+        if (count * 2 <= current) {
+            callback();
+            return;
+        }
+        flicker_quick(item, count, callback, current + 1)
+    }, 40);
+}
+
+function flicker_slow(item, count, callback, current) {
+    current = current || 0;
+    
+    $(item)[current % 2 == 0 ? "visible" : "invisible"]();
+    
+    setTimeout(function(){
+        if (count * 2 <= current) {
+            callback();
+            return;
+        }
+        flicker_slow(item, count, callback, current + 1)
+    }, 80);
+}
+
+function boom_flicker() {
+    $('.boom-text').css("opacity", ".25")
+    $('.boom-inner').css("opacity", ".25")
+    $('.boom-outer').css("opacity", ".25")
+
+    setTimeout(function () {
+        $('.boom-text').css("opacity", "1")
+    }, 1000);
+
+    setTimeout(function() {
+        $('.boom-inner').css("opacity", "1")
+    }, 2000);
+
+    setTimeout(function() {
+        $('.boom-outer').css("opacity", "1")
+    }, 3000);   
+
+    setTimeout(function() {
+        boom_flicker();
+    }, 4000);
+}
+
+
+
+
+// CUSTOM JQUERY VISIBILITY FUNCTIONS
+
+jQuery.fn.visible = function() {
+	return this.css('visibility', 'visible');
+};
+
+jQuery.fn.invisible = function() {
+	return this.css('visibility', 'hidden');
+};
+
+
+
+
+
+// START FLICKERING
+
+setTimeout(function () {
+    flicker_quick("#spark-corner", 2, function () {
+        $("#spark-corner").fadeIn("fast");
+    })
+
+    infinite_timer("#spark-corner");
+}, 1000)
+
+setTimeout(function () {
+    flicker_spark("#spark", 3, function () {
+        $("#spark").fadeIn("fast");
+    })
+}, 1000)
+
+setTimeout(function () {
+    flicker_slow(".bang", 4, function () {
+        $(".bang").fadeIn("fast");
+    })
+
+    infinite_timer(".bang");
+}, 950)
+
+setTimeout(function () {
+    flicker_slow(".chiddy", 4, function () {
+        $(".chiddy").fadeIn("fast");
+    })
+
+    infinite_timer(".chiddy");
+}, 1150)
+
+setTimeout(function () {
+    flicker_slow("#act-one", 4, function () {
+
+    })
+}, 1550)
+
+setTimeout(function () {
+    flicker_quick("#act-two", 4, function () {
+
+    })
+}, 2000)
+
+setTimeout(function () {
+    flicker_slow("#act-three", 4, function () {
+
+    })
+}, 2250)
+
+setTimeout(function () {
+    boom_flicker()
+}, 5000)
